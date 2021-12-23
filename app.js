df37425b5d7c11622a332072351d96aace25a41f
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2021-12-23 20:48:55 +0200

    init:html & css

---
+++ app.js
@@ -0,0 +1,5 @@
+const inputFile = document.getElementById("inputFile");
+const previewContainer = document.getElementById('imagePreview');
+const previewImage = previewContainer.querySelector('default-image')
+const previewText = previewContainer.querySelector('.image-default-text')
+
